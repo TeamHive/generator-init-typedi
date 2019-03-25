3119e4e2409ff8407f09658df3108df50527adb7
Tyler Vander Maas <tvmaasjazz@gmail.com>
2019-03-25 11:40:39 -0400

    feat: Merge package jsons and copy dot files

--- generators/app/index.ts
+++ generators/app/index.ts
@@ -1,6 +1,7 @@
 import * as download from 'download-git-repo';
 import * as fs from 'fs';
 import { kebabCase } from 'lodash';
+import * as sortPackageJson from 'sort-package-json';
 import * as Generator from 'yeoman-generator';
 import yosay = require('yosay');
 
@@ -14,6 +15,7 @@ module.exports = class extends Generator {
     initializing() {
         this.log(yosay('Start up a new TypeDI Project!'));
 
+        // download typedi-seed repo from github
         download('teamhive/typedi-seed', `${__dirname}/templates`, err => {
             if (err) {
                 // tslint:disable-next-line:no-console
@@ -21,10 +23,19 @@ module.exports = class extends Generator {
                 process.exit();
             }
 
-            fs.copyFileSync(`${__dirname}/_package.json`, `${__dirname}/templates/package.json`);
-            fs.copyFileSync(`${__dirname}/_README.md`, `${__dirname}/templates/README.md`);
+            // read both files into js objects
+            const seedPackage = require(`${__dirname}/templates/package.json`);
+            const generatorPackage = require(`${__dirname }/_package.json`);
 
-            return;
+            // combine package properties and sort
+            const combinedPackage = { ...seedPackage, ...generatorPackage };
+            const sortedPackage = sortPackageJson(combinedPackage);
+
+            // overwrite templates/package.json with combined package
+            fs.writeFileSync(`${__dirname}/templates/package.json`, JSON.stringify(sortedPackage, null, 4));
+
+            // overwrite seed README
+            fs.copyFileSync(`${__dirname}/_README.md`, `${__dirname}/templates/README.md`);
         });
     }
 
@@ -42,6 +53,7 @@ module.exports = class extends Generator {
             type: 'input',
             name: 'gitUser',
             message: 'GitHub user or organization',
+            default: 'TeamHive'
         }, {
             type: 'input',
             name: 'authorName',
@@ -57,9 +69,10 @@ module.exports = class extends Generator {
         this.fs.copyTpl(
             this.templatePath('**'),
             this.destinationPath(),
-            this.replacements()
+            this.replacements(),
+            {},
+            { globOptions: { dot: true } }
         );
-
     }
 
     install() {
